8ec6e190e705f8ec24410ec2f5a5530cce2120ac
Ehor <ehorlukianov@gmail.com>
2023-11-02 11:46:33 +0200

    init context

--- src/context.js
+++ src/context.js
@@ -1,1 +1,10 @@
 import React, { useState, useContext } from 'react'
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+    return <AppContext.Provider value="">{children}</AppContext.Provider>
+}
+
+
+export {AppContext, AppProvider};
