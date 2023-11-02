67b2d996ab161f6dfeb24ae142ddb084bbfdbc67
Ehor <ehorlukianov@gmail.com>
2023-11-02 11:56:08 +0200

    add functional button for open modal and sidebar in Home component

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,14 @@
 import React, { useContext } from 'react'
 import { FaBars } from 'react-icons/fa'
-
+import { AppContext } from './context'
 const Home = () => {
+  const {openSideBar, openModal} = useContext(AppContext);
   return(
   <main>
-    <button className='sidebar-toggle'>
+    <button className='sidebar-toggle' onClick={()=>openSideBar()}>
       <FaBars />
     </button>
-    <button className='btn'>Show modal</button>
+    <button className='btn' onClick={()=>openModal()}>Show modal</button>
   </main>
   )
 }
