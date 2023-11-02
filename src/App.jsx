d8362d4087b464ea3f730eec5186caae633e75b1
Ehor <ehorlukianov@gmail.com>
2023-11-02 11:37:45 +0200

    initial Home component

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,14 @@ import React from 'react'
 import Modal from './Modal'
 import Sidebar from './Sidebar'
 import Home from './Home'
-
 function App() {
-    return ( <
-        >
-        <
-        Home / >
-        <
-        Modal / >
-        <
-        Sidebar / >
-        <
-        />
-    )
+  return (
+    <>
+      <Home />
+      <Modal />
+      <Sidebar />
+    </>
+  )
 }
 
 export default App
