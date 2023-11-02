a9542e2af2c5d174573757bc5369b95f94ce4d66
Ehor <ehorlukianov@gmail.com>
2023-11-02 12:00:00 +0200

    add functional button for close sidebar in Sidebar component

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,14 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import logo from './logo.svg'
 import { FaTimes } from 'react-icons/fa'
 import { social, links } from './data'
-
+import { AppContext } from './context'
 const Sidebar = () => {
+  const {isSideBarOpen, closeSideBar} = useContext(AppContext);
   return (
-    <aside className={`sidebar`}>
+    <aside className={`sidebar ${isSideBarOpen && "show-sidebar"}`}>
       <div className="sidebar-header">
         <img src={logo} alt="logo" className="logo" />
-        <button className="close-btn">
+        <button className="close-btn" onClick={()=>closeSideBar()}>
           <FaTimes />
         </button>
       </div>
