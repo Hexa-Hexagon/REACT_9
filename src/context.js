e4780dce332c65a0dd49d2b7f14ea25aad46726f
Ehor <ehorlukianov@gmail.com>
2023-11-02 11:52:44 +0200

    add functional in context.js

--- src/context.js
+++ src/context.js
@@ -2,9 +2,35 @@ import React, { useState, useContext } from 'react'
 
 const AppContext = React.createContext();
 
+
 const AppProvider = ({ children }) => {
-    return <AppContext.Provider value="">{children}</AppContext.Provider>
+    const [isSideBarOpen, setIsSideBarOpen] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const openSideBar = () => {
+        setIsSideBarOpen(true);
+    };
+    const closeSideBar = () => {
+        setIsSideBarOpen(false);
+    };
+
+    const openModal = () => {
+        setIsModalOpen(true);
+    };
+    const closeModal = () => {
+        setIsModalOpen(false);
+    };
+
+    return <AppContext.Provider value = {
+        {
+            isModalOpen,
+            isSideBarOpen,
+            openModal,
+            openSideBar,
+            closeModal,
+            closeSideBar,
+        }
+    } > { children } < /AppContext.Provider>
 }
 
 
-export {AppContext, AppProvider};
+export { AppContext, AppProvider };
